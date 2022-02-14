8536f1fadcc4b8f1f99ea77f88975b52ed39cb03
aniltosun29 <85314429+aniltosun29@users.noreply.github.com>
2022-02-14 14:20:58 +0300

    Update normal parkour games.lua

--- normal parkour games.lua
+++ normal parkour games.lua
@@ -1,50 +1,99 @@
 local ScreenGui = Instance.new("ScreenGui")
 local main = Instance.new("Frame")
-local jumpon = Instance.new("TextButton")
-local jumpoff = Instance.new("TextButton")
-local hzon = Instance.new("TextButton")
-local hzoff = Instance.new("TextButton")
-local kapa = Instance.new("TextButton")
+local spdactivate = Instance.new("TextButton")
+local jmpactivate = Instance.new("TextButton")
+local spdtext = Instance.new("TextBox")
+local X = Instance.new("TextButton")
+local jmpdiss = Instance.new("TextButton")
+local ac = Instance.new("TextButton")
 
 ScreenGui.Parent = game.CoreGui
 
 main.Name = "main"
 main.Parent = ScreenGui
-main.BackgroundColor3 = Color3.fromRGB(175, 175, 175)
-main.Position = UDim2.new(0.540342331, 0, 0.284294248, 0)
-main.Size = UDim2.new(0, 298, 0, 217)
+main.BackgroundColor3 = Color3.fromRGB(70, 39, 4)
+main.Position = UDim2.new(0.243523329, 0, 0.226640165, 0)
+main.Size = UDim2.new(0, 228, 0, 191)
+main.Visible = false
 main.Active = true
 main.Draggable = true
 
-jumpon.Name = "jumpon"
-jumpon.Parent = main
-jumpon.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
-jumpon.Position = UDim2.new(0, 0, 0.769585252, 0)
-jumpon.Size = UDim2.new(0, 156, 0, 50)
-jumpon.Font = Enum.Font.SourceSans
-jumpon.Text = "Sonsuz zıplama aç"
-jumpon.TextColor3 = Color3.fromRGB(0, 0, 0)
-jumpon.TextSize = 14.000
-jumpon.MouseButton1Down:connect(function()
-	
+spdactivate.Name = "spdactivate"
+spdactivate.Parent = main
+spdactivate.BackgroundColor3 = Color3.fromRGB(79, 238, 238)
+spdactivate.Position = UDim2.new(0.540200949, 0, 0.111519352, 0)
+spdactivate.Size = UDim2.new(0, 83, 0, 64)
+spdactivate.Font = Enum.Font.SourceSans
+spdactivate.Text = "Activate"
+spdactivate.TextColor3 = Color3.fromRGB(0, 0, 0)
+spdactivate.TextSize = 14.000
+spdactivate.MouseButton1Down:connect(function()
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = main.spdtext.Text
+end)
+
+jmpactivate.Name = "jmpactivate"
+jmpactivate.Parent = main
+jmpactivate.BackgroundColor3 = Color3.fromRGB(79, 238, 238)
+jmpactivate.BorderColor3 = Color3.fromRGB(239, 239, 239)
+jmpactivate.BorderSizePixel = 0
+jmpactivate.Position = UDim2.new(0.540200889, 0, 0.603665948, 0)
+jmpactivate.Size = UDim2.new(0, 83, 0, 64)
+jmpactivate.Font = Enum.Font.SourceSans
+jmpactivate.Text = "Activate"
+jmpactivate.TextColor3 = Color3.fromRGB(0, 0, 0)
+jmpactivate.TextSize = 14.000
+jmpactivate.MouseButton1Down:connect(function()
+
 	InfiniteJumpEnabled = true
 	game:GetService("UserInputService").JumpRequest:connect(function()
 		if InfiniteJumpEnabled then
 			game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
 		end	
-     end)
+	end)
 end)
 
-jumpoff.Name = "jumpoff"
-jumpoff.Parent = main
-jumpoff.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
-jumpoff.Position = UDim2.new(0.523489952, 0, 0.769585252, 0)
-jumpoff.Size = UDim2.new(0, 142, 0, 50)
-jumpoff.Font = Enum.Font.SourceSans
-jumpoff.Text = "Sonsuz zıplama kapa"
-jumpoff.TextColor3 = Color3.fromRGB(0, 0, 0)
-jumpoff.TextSize = 14.000
-jumpoff.MouseButton1Down:connect(function()
+
+spdtext.Name = "spdtext"
+spdtext.Parent = main
+spdtext.BackgroundColor3 = Color3.fromRGB(79, 238, 238)
+spdtext.Position = UDim2.new(0.0622897893, 0, 0.110856727, 0)
+spdtext.Size = UDim2.new(0, 79, 0, 64)
+spdtext.Font = Enum.Font.SourceSans
+spdtext.Text = "speed value"
+spdtext.TextColor3 = Color3.fromRGB(0, 0, 0)
+spdtext.TextSize = 14.000
+
+X.Name = "X"
+X.Parent = main
+X.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+X.BackgroundTransparency = 1.000
+X.BorderSizePixel = 0
+X.Position = UDim2.new(0.903508782, 0, -0.0418848172, 0)
+X.Size = UDim2.new(0, 28, 0, 29)
+X.Font = Enum.Font.SourceSans
+X.LineHeight = 1.130
+X.Text = "X"
+X.TextColor3 = Color3.fromRGB(0, 0, 0)
+X.TextSize = 24.000
+X.TextStrokeColor3 = Color3.fromRGB(63, 63, 63)
+X.TextStrokeTransparency = 0.770
+X.MouseButton1Down:connect(function()
+	main.Visible = false
+	ac.Visible = true
+end)
+
+jmpdiss.Name = "jmpdiss"
+jmpdiss.Parent = main
+jmpdiss.BackgroundColor3 = Color3.fromRGB(79, 238, 238)
+jmpdiss.BorderColor3 = Color3.fromRGB(239, 239, 239)
+jmpdiss.BorderSizePixel = 0
+jmpdiss.Position = UDim2.new(0.0621307194, 0, 0.603665948, 0)
+jmpdiss.Size = UDim2.new(0, 83, 0, 64)
+jmpdiss.Font = Enum.Font.SourceSans
+jmpdiss.Text = "dis jump"
+jmpdiss.TextColor3 = Color3.fromRGB(0, 0, 0)
+jmpdiss.TextSize = 14.000
+jmpdiss.MouseButton1Down:connect(function()
 
 	InfiniteJumpEnabled = false
 	game:GetService("UserInputService").JumpRequest:connect(function()
@@ -54,42 +103,16 @@ jumpoff.MouseButton1Down:connect(function()
 	end)
 end)
 
-
-hzon.Name = "hızon"
-hzon.Parent = main
-hzon.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-hzon.Position = UDim2.new(0, 0, 0.539170504, 0)
-hzon.Size = UDim2.new(0, 156, 0, 50)
-hzon.Font = Enum.Font.SourceSans
-hzon.Text = "hızı aç"
-hzon.TextColor3 = Color3.fromRGB(0, 0, 0)
-hzon.TextSize = 14.000
-hzon.MouseButton1Down:connect(function()
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 400
-end)
-
-hzoff.Name = "hızoff"
-hzoff.Parent = main
-hzoff.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-hzoff.Position = UDim2.new(0.523489952, 0, 0.539170504, 0)
-hzoff.Size = UDim2.new(0, 142, 0, 50)
-hzoff.Font = Enum.Font.SourceSans
-hzoff.Text = "hızı kapa"
-hzoff.TextColor3 = Color3.fromRGB(0, 0, 0)
-hzoff.TextSize = 14.000
-hzoff.MouseButton1Down:connect(function()
-	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed =  16
-end)
-
-kapa.Name = "kapa"
-kapa.Parent = main
-kapa.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
-kapa.Position = UDim2.new(0, 0, 0.308755755, 0)
-kapa.Size = UDim2.new(0, 298, 0, 50)
-kapa.Font = Enum.Font.SourceSans
-kapa.Text = "menüyü kapat!"
-kapa.TextColor3 = Color3.fromRGB(0, 0, 0)
-kapa.TextSize = 14.000
-kapa.MouseButton1Down:connect(function()
-	main.Visible = not main.Visible;
+ac.Name = "ac"
+ac.Parent = ScreenGui
+ac.BackgroundColor3 = Color3.fromRGB(248, 35, 255)
+ac.Position = UDim2.new(0.52720207, 0, 0, 0)
+ac.Size = UDim2.new(0, 79, 0, 50)
+ac.Font = Enum.Font.SourceSans
+ac.Text = "hileyi ac"
+ac.TextColor3 = Color3.fromRGB(0, 0, 0)
+ac.MouseButton1Down:connect(function()
+ac.TextSize = 14.000
+main.Visible = true
+ac.Visible = false
 end)
